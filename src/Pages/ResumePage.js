1324b203a0e12ee9efe2bb88aad8f611e7c3e1cd
rldyd <kyleahn89@gmail.com>
2021-10-17 11:25:13 -0400

    Resume Page

--- src/Pages/ResumePage.js
+++ src/Pages/ResumePage.js
@@ -9,12 +9,13 @@ function ResumePage() {
       <div className="skillsContainer">
         <Skills skill={"HTML"} progress={"65%"} width={"65%"} />
         <Skills skill={"CSS"} progress={"70%"} width={"70%"} />
+        <Skills skill={"SCSS"} progress={"70%"} width={"70%"} />
         <Skills skill={"jQuery"} progress={"60%"} width={"60%"} />
-        <Skills skill={"Javascript"} progress={"80%"} width={"80%"} />
-        <Skills skill={"Node Js"} progress={"80%"} width={"80%"} />
-        <Skills skill={"Express"} progress={"75%"} width={"75%"} />
-        <Skills skill={"MongoDB"} progress={"75%"} width={"75%"} />
-        <Skills skill={"React Js"} progress={"80%"} width={"80%"} />
+        <Skills skill={"Javascript"} progress={"75%"} width={"75%"} />
+        <Skills skill={"Node Js"} progress={"70%"} width={"70%"} />
+        <Skills skill={"Express"} progress={"70%"} width={"70%"} />
+        <Skills skill={"MongoDB"} progress={"70%"} width={"70%"} />
+        <Skills skill={"React Js"} progress={"65%"} width={"65%"} />
       </div>
       <button className="btn">Dowonload Resume</button>
     </div>
